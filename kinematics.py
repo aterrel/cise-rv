2f43c72475c47c47acbf1b6ef07871621504b2ad
Matthew Rocklin <mrocklin@gmail.com>
2011-12-21 09:05:15 -0800

    added sampling example to kinematics.py

--- kinematics.py
+++ kinematics.py
@@ -1,22 +1,37 @@
 from sympy import *
 from sympy.statistics import *
 
-x0, y0, yf, m, th, t, g = symbols('x0 y0 yf m theta t g', real=True)
+x0, y0, yf, m, theta, t, g = symbols('x0 y0 y_f m theta t g', real=True)
 
-x = x0 + m*cos(th)*t
-y = y0 + m*sin(th)*t + g/2*t**2
+x = x0 + m*cos(theta)*t
+y = y0 + m*sin(theta)*t + g/2*t**2
 
 t_impact = solve(y-yf, t)[1]
 
 x_f = x.subs(t, t_impact)
 
-d = {x0:0, y0:10, yf:0, m:10, th:pi/4, g:S(-98)/10}
-dr = {x0:0, y0:Normal(10,1), yf:0, m:10, th:pi/4, g:S(-98)/10}
+d = {x0:0, y0:10, yf:0, m:10, theta:pi/4, g:S(-98)/10}
+dr = {x0:0, y0:Normal(10,1), yf:0, m:10, theta:pi/4, g:S(-98)/10}
 
-dr_big = {x0:0, y0:Normal(10,1, symbol=y0), yf:0, m:Normal(10,2, symbol=m), th:Normal(pi/4, pi/20, symbol=th), g:S(-98)/10}
+dr_big = {x0:0, y0:Normal(10,1, symbol=y0), yf:0, m:Normal(10,2, symbol=m),
+        theta:Normal(pi/4, pi/20, symbol=th), g:S(-98)/10}
 
 # Try
 # x_f.subs(d).evalf()
 # E(x_f.subs(dr))
 # P(x_f.subs(dr_big), samples=1000)
+
+def sampling_example():
+    y0 = Normal(10,1)
+    g = -9.8; x0=0; yf = 0
+    m = Uniform(9,11.5)
+    theta = Normal(pi/4, pi/20)
+
+    x = x0 + m*cos(theta)*t
+    y = y0 + m*sin(theta)*t + g/2*t**2
+    t_impact = solve(y-yf, t)[1]
+    x_f = x.subs(t, t_impact)
+
+    return P(theta>pi/4  ,  x_f>16, samples=1000)
+
 
